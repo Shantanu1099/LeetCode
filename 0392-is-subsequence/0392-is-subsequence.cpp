634188f5ca25f2f5b5f3ef3b2e2cb1bdc1776ad3
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2023-11-17 18:35:17 +0530

    Time: 3 ms (51.52%), Space: 6.8 MB (21.69%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        
+        // BRUTE FORCE T.C = O(N)        
         int idx = 0;
         for(int i = 0; i < t.size(); i++) {
             if(s[idx] == t[i]) idx++;
