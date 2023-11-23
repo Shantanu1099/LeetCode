9f5ce797e7f83d45ecb3312f4acc34e423d0af74
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2023-11-23 13:48:33 +0530

    Time: 25 ms (5.10%), Space: 10 MB (5.40%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        for(int i = 0, j = str.size() - 1 ; i < j; i++, j--) {
-            if(str[i] != str[j]) return false;
+        // string str = to_string(x);
+        // for(int i = 0, j = str.size() - 1 ; i < j; i++, j--) {
+        //     if(str[i] != str[j]) return false;
+        // }
+        
+        
+        
+        // Using vector for storing the values
+        int temp = x;
+        vector <int> x_val;
+        if(temp < 0) return false;
+        while(temp > 0) {
+            int rem = temp % 10;
+            x_val.push_back(rem);
+            temp /= 10;
+        }
+        for(int i = 0, j = x_val.size() - 1 ; i < j; i++, j--) {
+            if(x_val[i] != x_val[j]) return false;
         }
         return true;
     }
