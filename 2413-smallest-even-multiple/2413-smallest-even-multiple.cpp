65dad30c6a01b85ead00ad033e750dda90b82609
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2023-11-25 21:24:44 +0530

    Time: 2 ms (44.66%), Space: 6.4 MB (14.98%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
-    int smallestEvenMultiple(int n) {
-        for(int i = 1; i <= n*2; i++) {
-            if(i % n == 0 && i % 2 == 0) return i;
-        }
+    int smallestEvenMultiple(int n){
+        if(n % 2 == 0) return n;
         return n*2;
+        // if n is multiple of 2 it means it will also the multiple of n so return n
+        // if it is not multiple of 2 then multiply it with 2 and return so it will become multiple of 2 as well
+        
+        
+        // OR
+        // Short one liner for the above answer           
+        return n * (n % 2 + 1);
     }
 };
