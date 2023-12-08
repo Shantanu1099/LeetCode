6205297a77d38e3939db82f99d12735ae67cb9da
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2023-12-08 15:35:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (38.82%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sumOfNum = 0;
-        for(int i = 1 ; i < ((num/2) + 1); i++) {
-            if(num % i == 0) sumOfNum+=i;
-        }
-        return sumOfNum == num; 
+        // int sumOfNum = 0;
+        // for(int i = 1 ; i < ((num/2) + 1); i++) {
+        //     if(num % i == 0) sumOfNum+=i;
+        // }
+        // return sumOfNum == num; 
+        
+        
+        return ((num == 6) || (num == 28) || (num == 496) || (num == 8128) || (num == 33550336));
+
     }
 };
