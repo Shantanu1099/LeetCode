53df94d5e83012c083b491b46249f9b7bb079edd
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2023-12-08 16:02:42 +0530

    Time: 1065 ms (36.42%), Space: 6.2 MB (98.35%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        // int sumOfNum = 0;
-        // for(int i = 1 ; i < ((num/2) + 1); i++) {
-        //     if(num % i == 0) sumOfNum+=i;
-        // }
-        // return sumOfNum == num; 
-        
-        
-        return ((num == 6) || (num == 28) || (num == 496) || (num == 8128) || (num == 33550336));
-        // All perfect number - (2^(p − 1)) × (2^(p) − 1), where p is the prime number starting from 2 - Reference - https://en.wikipedia.org/wiki/List_of_Mersenne_primes_and_perfect_numbers
+        int sumOfNum = 0;
+        for(int i = 1 ; i < ((num/2) + 1); i++) {
+            if(num % i == 0) sumOfNum+=i;
+        }
+        return sumOfNum == num; 
     }
 };
