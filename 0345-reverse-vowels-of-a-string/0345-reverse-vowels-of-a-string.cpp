ebd5fa1bf7d3a24af5a757bb20029b88e555102f
Shantanu Singh <sshantanuthakur1099@gmail.com>
2024-04-11 19:24:49 +0530

    Time: 6 ms (66.79%), Space: 9 MB (80.86%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    bool isVowel(char &ch) {
+        return ( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ) ;
+    }
     string reverseVowels(string s) {
         int i = 0, j = s.length() - 1;
         while(i < j) {
             
-        bool isVowelI = (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' );
+//         bool isVowelI = (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' );
             
-        bool isVowelJ = (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U' );
+//         bool isVowelJ = (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U' );
             
-            if(!isVowelI) i++;
+            if(!isVowel(s[i])) i++;
             
-            else if(!isVowelJ) j--;
+            else if(!isVowel(s[j])) j--;
             
             else {
                 swap(s[i], s[j]);
